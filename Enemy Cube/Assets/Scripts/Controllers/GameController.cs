f639b458cb962343899de923f095a91a8a6ddfe2
Anderson <andersondez290@gmail.com>
2022-01-29 18:44:29 -0300

    Finalizado a parte de instanciar os objetos

--- Enemy Cube/Assets/Scripts/Controllers/GameController.cs
+++ Enemy Cube/Assets/Scripts/Controllers/GameController.cs
@@ -1,36 +1,43 @@
 using UnityEngine.UI;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject playerPrefab, enemyPrefab, enemyMidlePrefab, enemyHardPrefab;
+    public GameObject playerPrefab, enemyPrefab, enemyMidlePrefab, enemyHardPrefab, gemaPrefab;
     public Transform posPplayer;
+    public List<GameObject> posEnemy;
 
     private float time;
     private float timeSurvival;
-    
+    private float timeGema;
+    private int coutGema;
+
     // UI
     public GameObject menu;
-
+    public Text gema;
 
     void Start()
     {
         time = 0;
         timeSurvival = 0;
         menu.SetActive(false);
-        
+
         // ----
         InstantiatePlayer();
     }
 
     void Update()
     {
+
+        addGema();
         // check player gameover
         CheckGameOver();
 
         //times
         timeSurvival += Time.deltaTime;
         time += Time.deltaTime;
+        timeGema += Time.deltaTime;
 
         // enemy instantiate
         if (time >= 1 && !DeathPlayer.gameOver)
@@ -46,6 +53,13 @@ public class GameController : MonoBehaviour
             }
             time = 0;
         }
+
+        // gema instantiate
+        if (timeGema >= 2)
+        {
+            InstantiateGema();
+            timeGema = 0;
+        }
     }
 
     // ----
@@ -55,14 +69,8 @@ public class GameController : MonoBehaviour
     }
     void InstantiateEnemy(GameObject enemy)
     {
-        float posX = Random.Range(-8, 8);
-        float posY = Random.Range(-4, 4);
-        Vector3 posEnemy = new Vector3(posX, posY, 0);
-
-        if (Vector3.Distance(posEnemy, playerPrefab.transform.position) >= 3)
-        {
-            Instantiate(enemy, posEnemy, enemyPrefab.transform.rotation);
-        }
+        int drawPos = Random.Range(0, 8);
+        Instantiate(enemy, posEnemy[drawPos].transform.position, enemyPrefab.transform.rotation);
     }
     void CheckGameOver()
     {
@@ -70,5 +78,21 @@ public class GameController : MonoBehaviour
         {
             menu.SetActive(true);
         }
+    }
+    void addGema()
+    {
+        if (MovePlayer.isCollider)
+        {
+            coutGema += 1;
+            gema.text = coutGema.ToString();
+            MovePlayer.isCollider = false;
+        }
+    }
+    void InstantiateGema()
+    {
+        float posX = Random.Range(-7, 7);
+        float posY = Random.Range(-4, 4);
+        Vector3 posGema = new Vector3(posX, posY, 0);
+        Instantiate(gemaPrefab, posGema, gemaPrefab.transform.rotation);
     }
 }
--- Enemy Cube/Assets/Scripts/Player/MovePlayer.cs
+++ Enemy Cube/Assets/Scripts/Player/MovePlayer.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovePlayer : MonoBehaviour
 {
     float speed;
     Vector3 direction;
+    public static bool isCollider;
+
     void Start()
     {
         speed = 3.5f;
@@ -18,5 +21,14 @@ public class MovePlayer : MonoBehaviour
     {
         direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         transform.position += speed * direction * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Gema"))
+        {
+            isCollider = true;
+            Destroy(coll.gameObject);
+        }
     }
 }
